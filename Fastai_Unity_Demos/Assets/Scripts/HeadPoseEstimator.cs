e6a535023c2963e8bc5265093a22d288c4f7cbd9
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-05-23 22:24:52 -0700

    Update HeadPoseEstimator.cs

--- Fastai_Unity_Demos/Assets/Scripts/HeadPoseEstimator.cs
+++ Fastai_Unity_Demos/Assets/Scripts/HeadPoseEstimator.cs
@@ -1,5 +1,6 @@
 using Unity.Barracuda;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class HeadPoseEstimator : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class HeadPoseEstimator : MonoBehaviour
     public Vector2Int inputDims = new Vector2Int(320, 240);
     [Tooltip("The compute shader for GPU processing")]
     public ComputeShader processingShader;
+    [Tooltip("The material with the fragment shader for GPU processing")]
+    public Material processingMaterial;
 
     [Header("Barracuda")]
     [Tooltip("The Barracuda/ONNX asset file")]
@@ -22,7 +25,11 @@ public class HeadPoseEstimator : MonoBehaviour
     public int outputLayerIndex = 0;
     [Tooltip("EXPERIMENTAL: Indicate whether to order tensor data channels first")]
     public bool useNCHW = true;
-    
+
+    [Header("Output Processing")]
+    [Tooltip("Asynchronously download model output from the GPU to the CPU.")]
+    public bool useAsyncGPUReadback = true;
+
     [Header("Pose Visualization")]
     [Tooltip("The radius for the pose coordinate dot")]
     public float dotRadius = 10f;
@@ -32,6 +39,14 @@ public class HeadPoseEstimator : MonoBehaviour
     [Header("Debugging")]
     [Tooltip("Print debugging messages to the console")]
     public bool printDebugMessages = true;
+    [Tooltip("The on-screen text color")]
+    public Color textColor = Color.red;
+    [Tooltip("The scale value for the on-screen font size")]
+    [Range(0, 100)]
+    public int fontScale = 50;
+    [Tooltip("The number of seconds to wait between refreshing the fps value")]
+    [Range(0.01f, 1.0f)]
+    public float fpsRefreshRate = 0.1f;
 
     // The neural net model data structure
     private Model m_RunTimeModel;
@@ -48,11 +63,21 @@ public class HeadPoseEstimator : MonoBehaviour
     private RenderTexture inputTexture;
     // The source image dimensions
     private Vector2Int imageDims;
-    
+
+    // Stores the raw model output on the GPU when using useAsyncGPUReadback
+    private RenderTexture outputTextureGPU;
+    // Stores the raw model output on the CPU when using useAsyncGPUReadback
+    private Texture2D outputTextureCPU;
+
     // The predicted head pose coordinates
     private Vector2 coords;
     // The base texture for the pose estimation dot
     private Texture2D dotTexture;
+
+    // The current frame rate value
+    private int fps = 0;
+    // Controls when the frame rate value updates
+    private float fpsTimer = 0f;
 
 
     // Start is called before the first frame update
@@ -78,6 +103,33 @@ public class HeadPoseEstimator : MonoBehaviour
 
         // Initialize the texture for the head pose visualization
         dotTexture = Texture2D.whiteTexture;
+
+        // Initialize the GPU output texture
+        outputTextureGPU = RenderTexture.GetTemporary(1, 1, 24, RenderTextureFormat.ARGBHalf);
+        // Initialize the CPU output texture
+        outputTextureCPU = new Texture2D(1, 1, TextureFormat.RGBAHalf, false);
+    }
+
+    /// <summary>
+    /// Called once AsyncGPUReadback has been completed
+    /// </summary>
+    /// <param name="request"></param>
+    void OnCompleteReadback(AsyncGPUReadbackRequest request)
+    {
+        if (request.hasError)
+        {
+            Debug.Log("GPU readback error detected.");
+            return;
+        }
+
+        // Make sure the Texture2D is not null
+        if (outputTextureCPU)
+        {
+            // Fill Texture2D with raw data from the AsyncGPUReadbackRequest
+            outputTextureCPU.LoadRawTextureData(request.GetData<uint>());
+            // Apply changes to Textur2D
+            outputTextureCPU.Apply();
+        }
     }
 
     /// <summary>
@@ -89,15 +141,33 @@ public class HeadPoseEstimator : MonoBehaviour
     {
         // Get raw model output
         Tensor output = engine.PeekOutput(outputLayer);
+
         // Initialize vector for coordinates
         Vector2 coords = new Vector2();
 
-        // Process model output
-        for (int i=0; i<output.length; i++)
+        if (useAsyncGPUReadback)
         {
-            coords[i] = ((output[i] + 1) / 2) * inputDims[i] * (imageDims[i] / inputDims[i]);
+            // Copy model output to a RenderTexture
+            output.ToRenderTexture(outputTextureGPU);
+            // Asynchronously download model output from the GPU to the CPU
+            AsyncGPUReadback.Request(outputTextureGPU, 0, TextureFormat.RGBAHalf, OnCompleteReadback);
+
+            // Process model output
+            Color outputValues = outputTextureCPU.GetPixel(0, 0);
+            coords[0] = ((outputValues.r + 1) / 2) * inputDims[0] * (imageDims[0] / inputDims[0]);
+            coords[1] = ((outputValues.g + 1) / 2) * inputDims[1] * (imageDims[1] / inputDims[1]);
         }
-        if (this.printDebugMessages) Debug.Log($"Predicted Coords: {coords}");
+        else
+        {
+            // Process model output
+            for (int i = 0; i < output.length; i++)
+            {
+                coords[i] = ((output[i] + 1) / 2) * inputDims[i] * (imageDims[i] / inputDims[i]);
+            }
+        }
+
+                
+        if (printDebugMessages) Debug.Log($"Predicted Coords: {coords}");
 
         // Dispose Tensor and associated memories.
         output.Dispose();
@@ -111,11 +181,32 @@ public class HeadPoseEstimator : MonoBehaviour
     {
         // Copy the source image texture into model input texture
         Graphics.Blit(imageTexture, inputTexture);
-        // Normalize the input pixel data
-        Utils.ProcessImageGPU(inputTexture, processingShader, "NormalizeImageNet");
 
-        // Initialize a Tensor using the inputTexture
-        input = new Tensor(inputTexture, channels: 3);
+        if (SystemInfo.supportsComputeShaders)
+        {
+            // Normalize the input pixel data
+            Utils.ProcessImageGPU(inputTexture, processingShader, "NormalizeImageNet");
+            // Initialize a Tensor using the inputTexture
+            input = new Tensor(inputTexture, channels: 3);
+        }
+        else
+        {
+            // Disable asynchronous GPU readback when not using Compute Shaders
+            useAsyncGPUReadback = false;
+
+            // Define a temporary HDR RenderTexture
+            RenderTexture result = RenderTexture.GetTemporary(inputTexture.width,
+                inputTexture.height, 24, RenderTextureFormat.ARGBHalf);
+            RenderTexture.active = result;
+
+            // Apply preprocessing steps
+            Graphics.Blit(inputTexture, result, processingMaterial);
+
+            // Initialize a Tensor using the inputTexture
+            input = new Tensor(result, channels: 3);
+            RenderTexture.ReleaseTemporary(result);
+        }
+
         // Execute the model with the input Tensor
         engine.Execute(input);
         // Dispose Tensor and associated memories.
@@ -127,6 +218,20 @@ public class HeadPoseEstimator : MonoBehaviour
     // OnGUI is called for rendering and handling GUI events.
     public void OnGUI()
     {
+        if (Time.unscaledTime > fpsTimer)
+        {
+            fps = (int)(1f / Time.unscaledDeltaTime);
+            fpsTimer = Time.unscaledTime + fpsRefreshRate;
+        }
+
+        Rect fpsRect = new Rect(10, 10, 500, 500);
+
+        GUIStyle style = new GUIStyle();
+        style.fontSize = (int)(Screen.width * (1f / (100f - fontScale)));
+        style.normal.textColor = textColor;
+        
+        GUI.Label(fpsRect, new GUIContent($"FPS: {fps}"), style);
+
         // Get the smallest dimension of the target display
         float minDimension = Mathf.Min(Screen.width, Screen.height);
 
@@ -162,6 +267,8 @@ public class HeadPoseEstimator : MonoBehaviour
     private void OnDisable()
     {
         Destroy(inputTexture);
+
+        RenderTexture.ReleaseTemporary(outputTextureGPU);
 
         // Release the resources allocated for the inference engine
         engine.Dispose();
